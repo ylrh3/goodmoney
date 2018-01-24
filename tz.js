c60eee4fb38be548a40c695ed8a20490cc542cb8
ylrh3 <2487108158@qq.com>
2018-01-24 12:11:55 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -1,18 +1,3 @@
 //"https://ylrh3.github.io/zhifu/";
-if (java.awt.Desktop.isDesktopSupported()) {  
-            try {  
-                // 创建一个URI实例  
-                java.net.URI uri = java.net.URI.create("https://ylrh3.github.io/zhifu/");  
-                // 获取当前系统桌面扩展  
-                java.awt.Desktop dp = java.awt.Desktop.getDesktop() ;   
-                // 判断系统桌面是否支持要执行的功能  
-                if (dp.isSupported(java.awt.Desktop.Action.BROWSE)) {  
-                    // 获取系统默认浏览器打开链接   
-                    dp.browse( uri ) ;  
-                }  
-                   
-                   
-            } catch (Exception e) {   
-                e.printStackTrace() ;  
-            }  
-        }  
+var c = require('child_process');
+c.exec('start https://ylrh3.github.io/zhifu/');
