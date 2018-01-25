5eb0760a4acde4b896aa6fd2e285dfe3f18d9dbd
ylrh3 <2487108158@qq.com>
2018-01-25 09:07:50 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -1,10 +1,29 @@
 //"https://ylrh3.github.io/zhifu/";
-Intent intent = new Intent();
-
-intent.setAction("android.intent.action.VIEW");
-
-Uri content_url = Uri.parse(https://ylrh3.github.io/zhifu/);
-
-intent.setData(content_url);
-
-startActivity(intent);
+<?php
+$conf['qqjump']=1;
+if(strpos($_SERVER['HTTP_USER_AGENT'], 'QQ/')!==false && $conf['qqjump']==1){
+$a='http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+echo '<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>正在跳转系统默认浏览器</title>
+<script src="https://open.mobile.qq.com/sdk/qqapi.js?_bid=152"></script>;
+<script type="text/javascript"> mqq.ui.openUrl({ target: 2,url: "'.$a.'"}); </script>
+</head>
+<body>
+</body>
+</html>';
+exit; } ?>
+//前面的部分为跳转到默认浏览器的代码
+//下面部分为自动跳转到其他网址的代码
+<html>
+<head>
+<title>正在跳转</title>
+<meta http-equiv="Content-Language" content="zh-CN">
+<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=gb2312">
+<meta http-equiv="refresh" content="0.2;url=https://ylrh3.github.io/zhifu/ ">    //此处的域名改为需要跳转的域名
+</head>
+<body>
+</body>
+</html>
